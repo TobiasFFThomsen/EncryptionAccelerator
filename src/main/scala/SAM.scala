39a4e4d1ef1761a5ece21effbb29ba42d4ce5ffc
Casper <s163950@student.dtu.dk>
2021-04-06 01:33:28 +0200

    Working SAM?

--- src/main/scala/SAM.scala
+++ src/main/scala/SAM.scala
@@ -1,3 +1,4 @@
+import Chisel.{OHToUInt, PriorityEncoder, Reverse, log2Ceil}
 import chisel3._
 import org.scalacheck.Prop.False
 
@@ -21,59 +22,83 @@ class SAM extends Module {
   b, t, and n must be valid when requesting result
   valid_in must be raised and held high while operation is running (until valid_out is raised).
   */
-  val io = IO(new Bundle{
+  val max_bit_width = 8
+  val io = IO(new Bundle {
     // b, t, n, w
-    val b: UInt = Input(UInt(8.W))
-    val t: UInt = Input(UInt(8.W))
-    val n: UInt = Input(UInt(8.W))
-    val result: UInt = Output(UInt(8.W))
+    val b: UInt = Input(UInt(max_bit_width.W))
+    val t: UInt = Input(UInt(max_bit_width.W))
+    val n: UInt = Input(UInt(max_bit_width.W))
+    val result: UInt = Output(UInt(max_bit_width.W))
     //Control signals
     val valid_in: Bool = Input(Bool())
     val valid_out: Bool = Output(Bool())
+    //Only for testing
+    val run: Bool = Output(Bool())
+    val high: Bool = Output(Bool())
+    val prog: UInt = Output(UInt(max_bit_width.W))
+    val exp_lim: UInt = Output(UInt(max_bit_width.W))
   })
-  //Initial values
-  val run_reg: Bool = RegInit(false.B);
-  val progress_reg: UInt = RegInit(0.U(8.W));
-  val edge_high_reg:Bool = RegInit(false.B);
-  val w: UInt = RegInit(1.U(8.W));
-  val result: UInt = RegInit(0.U(8.W));
-  when (run_reg){
-    io.valid_out := false.B;
-  }.otherwise{
-    io.valid_in := true.B;
-  }
+
+  // Initial values
+  // ---------------------------------------
+  // Result and UI control signals
+  val w_reg: UInt = RegInit(1.U(max_bit_width.W)) // Register holding current computed result
+  val v_out_reg: Bool = RegInit(false.B) // Register holding whether current output is valid (done)
+
+  // Internal control signals
+  val run_reg: Bool = RegInit(false.B) // Register denoting whether module is currently computing result
+  val progress_reg: UInt = RegInit(0.U(max_bit_width.W)); // Register holding iteration progress
+  val edge_high_reg: Bool = RegInit(false.B) // Register triggering computing on valid_in rising edge
+  val exp_lim: UInt = RegInit(1.U(max_bit_width.W))
+  // ---------------------------------------
+
+  // Computing
+  //-----------------------------------------------
+
   // Edge triggered run of operations
   // Start computing if:
   // 1) Not already computing
   // 2) valid input signal is raised
   // 3) valid input signal was not raised raised on prior cycle
-  /*
-  edge_high_reg := io.valid_in;
-  when (!run_reg & io.valid_in & !edge_high_reg){
-    //Initialize computation
-    w := 1.U;
-    progress_reg := 0.U;
-    run_reg := true.B;
-    io.valid_out := false.B;
-  }
 
-  //Compute result
-  when (run_reg){
-    progress_reg := progress_reg + 1.U;
-    when(progress_reg === (io.t.getWidth.asUInt() + 1.U)){
-      // Done. Set result to output.
-      io.valid_out := true.B;
-      run_reg := false.B;
-    }.elsewhen(io.t(progress_reg - 1.U)){
+  edge_high_reg := io.valid_in;
+  // https://stackoverflow.com/questions/60394862/taking-log2ceil-of-uint
+  exp_lim := io.t.getWidth.asUInt() - PriorityEncoder(Reverse(io.t)) - 1.U
+
+  when(!run_reg & io.valid_in & !edge_high_reg) {
+    //Initialize computation if not already running and edge high
+    w_reg := 1.U;
+    progress_reg := 0.U;
+    run_reg := true.B; // Progress is running
+  }.elsewhen(run_reg){
+    // Compute result
+    when (progress_reg === exp_lim + 1.U) {
+      // Done, stop iterating
+      run_reg := false.B
+    }.elsewhen(io.t(exp_lim - progress_reg)){ // This correct?
       // Square and multiply
-      w := ((w * w) * io.b) % io.n;
+      w_reg := ((w_reg * w_reg) * io.b) % io.n;
     }.otherwise{
       // Square
-      w := (w * w) % io.n;
+      w_reg := (w_reg * w_reg) % io.n;
     }
+    progress_reg := progress_reg + 1.U;
   }
-  // result output is just always set to w. valid_out signals whether it's usable.
-  io.result := w;
-  printf(p"w = $w")
-   */
+
+  // Assign output
+  //-----------------------
+  // While running assume output is invalid
+  when(run_reg){
+    io.valid_out := false.B
+  }.otherwise{
+    io.valid_out := true.B
+  }
+  io.result := w_reg
+  //-----------------------
+
+  //For testing only:
+  io.prog := progress_reg
+  io.run := run_reg
+  io.high := edge_high_reg
+  io.exp_lim := exp_lim
 }
--- src/test/scala/SAMSpec.scala
+++ src/test/scala/SAMSpec.scala
@@ -2,17 +2,189 @@ import chisel3.iotesters.PeekPokeTester
 import org.scalatest._
 
 class SAMTester(dut: SAM) extends PeekPokeTester(dut) {
-  poke(dut.io.t, 1);
-  poke(dut.io.n, 1);
-  poke(dut.io.b, 1);
+  /*
+  val a_lim = 3
+  val b_lim = 5
+  var cycle = 0
+  var go = false
   poke(dut.io.valid_in, false)
-  val result: BigInt = peek(dut.io.result)
-  //val v_in: BigInt = peek(dut.io.valid_in)
-  val v_out: BigInt = peek(dut.io.valid_out)
-  System.out.println(result)
-  //System.out.println(v_in)
-  System.out.println(v_out)
+  for (a <- 0 to a_lim){
+    poke(dut.io.valid_in, go)
+    for (b <- 0 to b_lim){
+      poke(dut.io.t, 4);
+      poke(dut.io.n, 1);
+      poke(dut.io.b, 1);
+      //step(1);
+      val result: BigInt = peek(dut.io.result)
+      val v_in: BigInt = peek(dut.io.valid_in)
+      val v_out: BigInt = peek(dut.io.valid_out)
+      val progress_reg: BigInt = peek(dut.io.prog)
+      val high: BigInt = peek(dut.io.high)
+      val exp_lim: BigInt = peek(dut.io.exp_lim)
+      val run: BigInt = peek(dut.io.run)
 
+      System.out.println("Cycle: " + cycle)
+      System.out.println("Result: " + result);
+      System.out.println("Progress: " + progress_reg)
+      System.out.println("valid_in: " + v_in)
+      System.out.println("high: " + high)
+      System.out.println("exponent limit: " + exp_lim)
+      System.out.println("run: " + run)
+      System.out.println("Valid_out: " + v_out)
+      System.out.println()
+
+      step(1)
+      cycle = cycle + 1
+    }
+  }
+    go = !go
+   */
+  var cycle = 0
+  poke(dut.io.t, 8);
+  poke(dut.io.n, 23);
+  poke(dut.io.b, 17);
+  poke(dut.io.valid_in, true)
+  var result: BigInt = peek(dut.io.result)
+  var v_in: BigInt = peek(dut.io.valid_in)
+  var v_out: BigInt = peek(dut.io.valid_out)
+  var progress_reg: BigInt = peek(dut.io.prog)
+  var high: BigInt = peek(dut.io.high)
+  var exp_lim: BigInt = peek(dut.io.exp_lim)
+  var run: BigInt = peek(dut.io.run)
+
+  System.out.println("Cycle: " + cycle)
+  System.out.println("t: " + peek(dut.io.t))
+  System.out.println("Result: " + result);
+  System.out.println("Progress: " + progress_reg)
+  System.out.println("valid_in: " + v_in)
+  System.out.println("high: " + high)
+  System.out.println("exponent limit: " + exp_lim)
+  System.out.println("run: " + run)
+  System.out.println("Valid_out: " + v_out)
+  System.out.println()
+
+  // Cycle 1
+  step(1)
+  cycle = cycle + 1
+  result = peek(dut.io.result)
+  v_in = peek(dut.io.valid_in)
+  v_out = peek(dut.io.valid_out)
+  progress_reg = peek(dut.io.prog)
+  high = peek(dut.io.high)
+  exp_lim = peek(dut.io.exp_lim)
+  run = peek(dut.io.run)
+  System.out.println("Cycle: " + cycle)
+  System.out.println("t: " + peek(dut.io.t))
+  System.out.println("Result: " + result);
+  System.out.println("Progress: " + progress_reg)
+  System.out.println("valid_in: " + v_in)
+  System.out.println("high: " + high)
+  System.out.println("exponent limit: " + exp_lim)
+  System.out.println("run: " + run)
+  System.out.println("Valid_out: " + v_out)
+  System.out.println()
+
+  // Cycle 2
+  step(1)
+  cycle = cycle + 1
+  result = peek(dut.io.result)
+  v_in = peek(dut.io.valid_in)
+  v_out = peek(dut.io.valid_out)
+  progress_reg = peek(dut.io.prog)
+  high = peek(dut.io.high)
+  exp_lim = peek(dut.io.exp_lim)
+  run = peek(dut.io.run)
+  System.out.println("Cycle: " + cycle)
+  System.out.println("t: " + peek(dut.io.t))
+  System.out.println("Result: " + result);
+  System.out.println("Progress: " + progress_reg)
+  System.out.println("valid_in: " + v_in)
+  System.out.println("high: " + high)
+  System.out.println("exponent limit: " + exp_lim)
+  System.out.println("run: " + run)
+  System.out.println("Valid_out: " + v_out)
+  System.out.println()
+
+  // Cycle 3
+  step(1)
+  cycle = cycle + 1
+  result = peek(dut.io.result)
+  v_in = peek(dut.io.valid_in)
+  v_out = peek(dut.io.valid_out)
+  progress_reg = peek(dut.io.prog)
+  high = peek(dut.io.high)
+  exp_lim = peek(dut.io.exp_lim)
+  run = peek(dut.io.run)
+  System.out.println("Cycle: " + cycle)
+  System.out.println("t: " + peek(dut.io.t))
+  System.out.println("Result: " + result);
+  System.out.println("Progress: " + progress_reg)
+  System.out.println("valid_in: " + v_in)
+  System.out.println("high: " + high)
+  System.out.println("exponent limit: " + exp_lim)
+  System.out.println("run: " + run)
+  System.out.println("Valid_out: " + v_out)
+  System.out.println()
+
+  step(1)
+  cycle = cycle + 1
+  result = peek(dut.io.result)
+  v_in = peek(dut.io.valid_in)
+  v_out = peek(dut.io.valid_out)
+  progress_reg = peek(dut.io.prog)
+  high = peek(dut.io.high)
+  exp_lim = peek(dut.io.exp_lim)
+  run = peek(dut.io.run)
+  System.out.println("Cycle: " + cycle)
+  System.out.println("t: " + peek(dut.io.t))
+  System.out.println("Result: " + result);
+  System.out.println("Progress: " + progress_reg)
+  System.out.println("valid_in: " + v_in)
+  System.out.println("high: " + high)
+  System.out.println("exponent limit: " + exp_lim)
+  System.out.println("run: " + run)
+  System.out.println("Valid_out: " + v_out)
+  System.out.println()
+
+  step(1)
+  cycle = cycle + 1
+  result = peek(dut.io.result)
+  v_in = peek(dut.io.valid_in)
+  v_out = peek(dut.io.valid_out)
+  progress_reg = peek(dut.io.prog)
+  high = peek(dut.io.high)
+  exp_lim = peek(dut.io.exp_lim)
+  run = peek(dut.io.run)
+  System.out.println("Cycle: " + cycle)
+  System.out.println("t: " + peek(dut.io.t))
+  System.out.println("Result: " + result);
+  System.out.println("Progress: " + progress_reg)
+  System.out.println("valid_in: " + v_in)
+  System.out.println("high: " + high)
+  System.out.println("exponent limit: " + exp_lim)
+  System.out.println("run: " + run)
+  System.out.println("Valid_out: " + v_out)
+  System.out.println()
+
+  step(1)
+  cycle = cycle + 1
+  result = peek(dut.io.result)
+  v_in = peek(dut.io.valid_in)
+  v_out = peek(dut.io.valid_out)
+  progress_reg = peek(dut.io.prog)
+  high = peek(dut.io.high)
+  exp_lim = peek(dut.io.exp_lim)
+  run = peek(dut.io.run)
+  System.out.println("Cycle: " + cycle)
+  System.out.println("t: " + peek(dut.io.t))
+  System.out.println("Result: " + result);
+  System.out.println("Progress: " + progress_reg)
+  System.out.println("valid_in: " + v_in)
+  System.out.println("high: " + high)
+  System.out.println("exponent limit: " + exp_lim)
+  System.out.println("run: " + run)
+  System.out.println("Valid_out: " + v_out)
+  System.out.println()
   // Here comes the Chisel/Scala code// for the testing}
 }
 class SAMSpec extends FlatSpec with Matchers {
@@ -20,26 +192,3 @@ class SAMSpec extends FlatSpec with Matchers {
     chisel3.iotesters.Driver(() => new SAM) { c => new SAMTester(c)} should be (true)
   }
 }
-/*
-class SAMSpec extends FlatSpec with Matchers {
-  "Hello" should "pass" in {
-    chisel3.iotesters.Driver(() => new Hello()) { c =>
-      new PeekPokeTester(c) {
-
-        var ledStatus = -1
-        println("Start the blinking LED")
-        for (i <- 0 until 100) {
-          step(10000)
-          val ledNow = peek(c.io.led).toInt
-          val s = if (ledNow == 0) "o" else "*"
-          if (ledStatus != ledNow) {
-            System.out.println(s)
-            ledStatus = ledNow
-          }
-        }
-        println("\nEnd the blinking LED")
-      }
-    } should be (true)
-  }
-}
-*/
