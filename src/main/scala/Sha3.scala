db67f35fa94bb1c72c591a2532fef59e0c160827
Tobias Thomsen <tobias@debian.tobias_debian>
2021-05-16 21:44:31 +0200

    nearlyThere

--- src/main/scala/Sha3.scala
+++ src/main/scala/Sha3.scala
@@ -2,47 +2,78 @@ import chisel3._
 
 class Sha3 extends Module {
   val io            = IO( new Bundle {
-    val iota_round = Input(UInt(64.W))
-    val r_in    = Input(Vec(9, UInt(64.W)))
-    val c_in    = Input(Vec(16,UInt(64.W)))
-    val r_out   = Output(Vec(9, UInt(64.W)))
-    val c_out   = Output(Vec(16, UInt(64.W)))
+
+    val iota_round  = Input(UInt(64.W))
+    val r_in        = Input(Vec(9, UInt(64.W)))
+    val c_in        = Input(Vec(16,UInt(64.W)))
+    val buffer_ready = Input(Bool())
 
     // For testing:
+    val round_out = Output(Vec(5,Vec(5,UInt(64.W))))
     val theta_out = Output(Vec(5,Vec(5,UInt(64.W))))
     val theta_d   = Output(Vec(5,UInt(64.W)))
     val theta_c   = Output(Vec(5,UInt(64.W)))
-
-
     val rhoPi_out = Output(Vec(5,Vec(5,UInt(64.W))))
     val chi_out   = Output(Vec(5,Vec(5,UInt(64.W))))
     val iota_out  = Output(UInt(64.W))
+    val register_in   = Input(Vec(5,Vec(5,UInt(64.W))))
+    val register_out  = Output(Vec(5,Vec(5,UInt(64.W))))
 
   })
 
-  val round = Module(new Round())
+  val round           = Module(new Round())
+  val stateRegister   = Module(new StateRegister())
 
-  io.theta_d := round.io.R_theta_d_out
-  io.theta_c := round.io.R_theta_c_out
+  // Signals for testing
+  io.theta_d    := round.io.R_theta_d_out
+  io.theta_c    := round.io.R_theta_c_out
+  io.theta_out  := round.io.R_theta_out
+  io.rhoPi_out  := round.io.R_rhoPi_out
+  io.chi_out    := round.io.R_chi_out
+  io.iota_out   := round.io.R_iota_out
+  stateRegister.io.registerIn := io.register_in
+  io.register_out := stateRegister.io.registerOut
 
-  io.theta_out := round.io.R_theta_out
-  io.rhoPi_out := round.io.R_rhoPi_out
-  io.chi_out   := round.io.R_chi_out
-  io.iota_out  := round.io.R_iota_out
 
   // The iota round tells the iota module what number in the table it should xor with.
   round.io.R_iota_round := io.iota_round
 
+  /*
+    poke(dut.io.r_in(0),stateRegister(0)(0))
+    poke(dut.io.r_in(1),stateRegister(0)(1))
+    poke(dut.io.r_in(2),stateRegister(0)(2))
+    poke(dut.io.r_in(3),stateRegister(0)(3))
+    poke(dut.io.r_in(4),stateRegister(0)(4))
+    poke(dut.io.r_in(5),stateRegister(1)(0))
+    poke(dut.io.r_in(6),stateRegister(1)(1))
+    poke(dut.io.r_in(7),stateRegister(1)(2))
+    poke(dut.io.r_in(8),stateRegister(1)(3))
+   */
+
+
+
   // Rate (r) =  9 * 64 = 576 bits
-  round.io.round_in(0)(0) := io.r_in(0)
-  round.io.round_in(1)(0) := io.r_in(1)
-  round.io.round_in(2)(0) := io.r_in(2)
-  round.io.round_in(3)(0) := io.r_in(3)
-  round.io.round_in(4)(0) := io.r_in(4)
-  round.io.round_in(0)(1) := io.r_in(5)
-  round.io.round_in(1)(1) := io.r_in(6)
-  round.io.round_in(2)(1) := io.r_in(7)
-  round.io.round_in(3)(1) := io.r_in(8)
+  when(io.buffer_ready){
+    round.io.round_in(0)(0) := io.r_in(0)^stateRegister.io.registerOut(0)(0)
+    round.io.round_in(1)(0) := io.r_in(1)^stateRegister.io.registerOut(0)(1)
+    round.io.round_in(2)(0) := io.r_in(2)^stateRegister.io.registerOut(0)(2)
+    round.io.round_in(3)(0) := io.r_in(3)^stateRegister.io.registerOut(0)(3)
+    round.io.round_in(4)(0) := io.r_in(4)^stateRegister.io.registerOut(0)(4)
+    round.io.round_in(0)(1) := io.r_in(5)^stateRegister.io.registerOut(1)(0)
+    round.io.round_in(1)(1) := io.r_in(6)^stateRegister.io.registerOut(1)(1)
+    round.io.round_in(2)(1) := io.r_in(7)^stateRegister.io.registerOut(1)(2)
+    round.io.round_in(3)(1) := io.r_in(8)^stateRegister.io.registerOut(1)(3)
+  }.otherwise{
+    round.io.round_in(0)(0) := io.r_in(0)
+    round.io.round_in(1)(0) := io.r_in(1)
+    round.io.round_in(2)(0) := io.r_in(2)
+    round.io.round_in(3)(0) := io.r_in(3)
+    round.io.round_in(4)(0) := io.r_in(4)
+    round.io.round_in(0)(1) := io.r_in(5)
+    round.io.round_in(1)(1) := io.r_in(6)
+    round.io.round_in(2)(1) := io.r_in(7)
+    round.io.round_in(3)(1) := io.r_in(8)
+  }
 
   // Capacity (c) = 16*64 = 1024 bits
   round.io.round_in(4)(1) := io.c_in(0)
@@ -62,35 +93,14 @@ class Sha3 extends Module {
   round.io.round_in(3)(4) := io.c_in(14)
   round.io.round_in(4)(4) := io.c_in(15)
 
-  io.r_out(0) := round.io.round_out(0)(0)
-  io.r_out(1) := round.io.round_out(1)(0)
-  io.r_out(2) := round.io.round_out(2)(0)
-  io.r_out(3) := round.io.round_out(3)(0)
-  io.r_out(4) := round.io.round_out(4)(0)
-  io.r_out(5) := round.io.round_out(0)(1)
-  io.r_out(6) := round.io.round_out(1)(1)
-  io.r_out(7) := round.io.round_out(2)(1)
-  io.r_out(8) := round.io.round_out(3)(1)
+  for(x <- 0 to 4){
+    for(y <- 0 to 4){
+      io.round_out(x)(y) := round.io.round_out(y)(x)
+    }
+  }
 
-  io.c_out(0) := round.io.round_out(4)(1)
-
-  io.c_out(1) := round.io.round_out(0)(2)
-  io.c_out(2) := round.io.round_out(1)(2)
-  io.c_out(3) := round.io.round_out(2)(2)
-  io.c_out(4) := round.io.round_out(3)(2)
-  io.c_out(5) := round.io.round_out(4)(2)
-
-  io.c_out(6) := round.io.round_out(0)(3)
-  io.c_out(7) := round.io.round_out(1)(3)
-  io.c_out(8) := round.io.round_out(2)(3)
-  io.c_out(9) := round.io.round_out(3)(3)
-  io.c_out(10) := round.io.round_out(4)(3)
-
-  io.c_out(11) := round.io.round_out(0)(4)
-  io.c_out(12) := round.io.round_out(1)(4)
-  io.c_out(13) := round.io.round_out(2)(4)
-  io.c_out(14) := round.io.round_out(3)(4)
-  io.c_out(15) := round.io.round_out(4)(4)
+  stateRegister.io.registerIn := round.io.round_out
+  round.io.round_in           := stateRegister.io.registerOut
 
 }
 
--- src/test/scala/round_spec.scala
+++ src/test/scala/round_spec.scala
@@ -4,41 +4,40 @@ import org.scalatest._
 
 class round_tester(dut: Sha3) extends PeekPokeTester(dut)  {
 
-  val pokeState: Array[Array[BigInt]] = Array.ofDim[BigInt](5, 5)
+  val stateRegister: Array[Array[BigInt]] = Array.ofDim[BigInt](5, 5)
 
-  initPokeState()
+  initStateRegister()
+  printState("Output", "Verbose")
 
   for(i <- 0 to 23) {
     System.out.println("ROUND "+i)
 
-    // 6. Set the iota to i
+    // 1. Set the iota to i
     poke(dut.io.iota_round,i.U)
 
-    // 7. Input the output of the last calculation to the round (r_in and c_in)
-    loadState()
+    // 2. Input the output of the last calculation to the round (r_in and c_in)
     printState("Input", "")
 
-    // 4. Peek at the result (this is supposed to be the result of the first round)
+    // 3. Peek at the result (this is supposed to be the result of the first round)
     printState("Output", "Verbose")
 
-    // 5. Store the output of this calculation in some variables (the r_out and c_out)
-    storeState()
+    // 4. Store the output of this calculation in some variables (the r_out and c_out)
   }
 
 
   // Helper functions
   def printState(s:String, arg:String):Unit={
     System.out.println("\n\n PRINTING "+s+" OF ROUND")
-    val peekState = Array.ofDim[String](5, 5)
-    val thetaOut  = Array.ofDim[String](5, 5)
-    val rhoPiOut  = Array.ofDim[String](5, 5)
-    val iotaOut   = Array.ofDim[String](5, 5)
-    val chiOut    = Array.ofDim[String](5, 5)
+    val peekState     = Array.ofDim[String](5, 5)
+    val thetaOut      = Array.ofDim[String](5, 5)
+    val rhoPiOut      = Array.ofDim[String](5, 5)
+    val iotaOut       = Array.ofDim[String](5, 5)
+    val chiOut        = Array.ofDim[String](5, 5)
+    val register_out  = Array.ofDim[String](5, 5)
     val theta_d   = Array.ofDim[String](5)
     val theta_c   = Array.ofDim[String](5)
 
     if(s.contentEquals("Input")){
-
       peekState(0)(0) = peek(dut.io.r_in(0)).toString(16)
       peekState(0)(1) = peek(dut.io.r_in(1)).toString(16)
       peekState(0)(2) = peek(dut.io.r_in(2)).toString(16)
@@ -59,22 +58,38 @@ class round_tester(dut: Sha3) extends PeekPokeTester(dut)  {
       peekState(3)(1) = peek(dut.io.c_in(7)).toString(16)
       peekState(3)(2) = peek(dut.io.c_in(8)).toString(16)
       peekState(3)(3) = peek(dut.io.c_in(9)).toString(16)
-      peekState(3)(4) = peek(dut.io.c_in(10)).toString(16) //error
+      peekState(3)(4) = peek(dut.io.c_in(10)).toString(16)
       peekState(4)(0) = peek(dut.io.c_in(11)).toString(16)
       peekState(4)(1) = peek(dut.io.c_in(12)).toString(16)
       peekState(4)(2) = peek(dut.io.c_in(13)).toString(16)
       peekState(4)(3) = peek(dut.io.c_in(14)).toString(16)
       peekState(4)(4) = peek(dut.io.c_in(15)).toString(16)
+
+      System.out.println("Register out (Round in)")
+      System.out.println("\n")
+
+      for(x <- 0 to 4)
+        System.out.println(
+          register_out(x)(0)+
+            "\t"+register_out(x)(1)+
+            "\t"+register_out(x)(2)+
+            "\t"+register_out(x)(3)+
+            "\t"+register_out(x)(4)
+        )
+      System.out.println("\n")
+
     }
     if(s.contentEquals("Output")){
 
       if(arg.contentEquals("Verbose")){
 
-        for(x <- 0 to 4) {
-          for(y <- 0 to 4) {
+        for(x <- 0 to 4)
+          for(y <- 0 to 4)
+            register_out(x)(y) = peek(dut.io.register_out(x)(y)).toString(16)
+
+       for(x <- 0 to 4)
+          for(y <- 0 to 4)
             thetaOut(x)(y) = peek(dut.io.theta_out(x)(y)).toString(16)
-          }
-        }
 
         for(i <- 0 to 4)
           theta_c(i) = peek(dut.io.theta_c(i)).toString(16)
@@ -82,54 +97,24 @@ class round_tester(dut: Sha3) extends PeekPokeTester(dut)  {
         for(i <- 0 to 4)
           theta_d(i) = peek(dut.io.theta_d(i)).toString(16)
 
-        for(x <- 0 to 4) {
-          for(y <- 0 to 4) {
+        for(x <- 0 to 4)
+          for(y <- 0 to 4)
             rhoPiOut(x)(y) = peek(dut.io.rhoPi_out(x)(y)).toString(16)
-          }
-        }
 
-        for(x <- 0 to 4) {
-          for(y <- 0 to 4) {
+        for(x <- 0 to 4)
+          for(y <- 0 to 4)
             chiOut(x)(y) = peek(dut.io.chi_out(x)(y)).toString(16)
-          }
-        }
 
         for(x <- 0 to 4) {
           for(y <- 0 to 4) {
             iotaOut(x)(y) = peek(dut.io.rhoPi_out(x)(y)).toString(16)
           }
-        }
         iotaOut(0)(0) = peek(dut.io.iota_out).toString(16)
       }
-      peekState(0)(0) = peek(dut.io.r_out(0)).toString(16)
-      peekState(0)(1) = peek(dut.io.r_out(1)).toString(16)
-      peekState(0)(2) = peek(dut.io.r_out(2)).toString(16)
-      peekState(0)(3) = peek(dut.io.r_out(3)).toString(16)
-      peekState(0)(4) = peek(dut.io.r_out(4)).toString(16)
-      peekState(1)(0) = peek(dut.io.r_out(5)).toString(16)
-      peekState(1)(1) = peek(dut.io.r_out(6)).toString(16)
-      peekState(1)(2) = peek(dut.io.r_out(7)).toString(16)
-      peekState(1)(3) = peek(dut.io.r_out(8)).toString(16)
 
-      peekState(1)(4) = peek(dut.io.c_out(0)).toString(16)
-
-      peekState(2)(0) = peek(dut.io.c_out(1)).toString(16)
-      peekState(2)(1) = peek(dut.io.c_out(2)).toString(16)
-      peekState(2)(2) = peek(dut.io.c_out(3)).toString(16)
-      peekState(2)(3) = peek(dut.io.c_out(4)).toString(16)
-      peekState(2)(4) = peek(dut.io.c_out(5)).toString(16)
-
-      peekState(3)(0) = peek(dut.io.c_out(6)).toString(16)
-      peekState(3)(1) = peek(dut.io.c_out(7)).toString(16)
-      peekState(3)(2) = peek(dut.io.c_out(8)).toString(16)
-      peekState(3)(3) = peek(dut.io.c_out(9)).toString(16)
-      peekState(3)(4) = peek(dut.io.c_out(10)).toString(16)
-
-      peekState(4)(0) = peek(dut.io.c_out(11)).toString(16)
-      peekState(4)(1) = peek(dut.io.c_out(12)).toString(16)
-      peekState(4)(2) = peek(dut.io.c_out(13)).toString(16)
-      peekState(4)(3) = peek(dut.io.c_out(14)).toString(16)
-      peekState(4)(4) = peek(dut.io.c_out(15)).toString(16)
+      for(x <- 0 to 4)
+        for(y <- 0 to 4)
+          peekState(x)(y) = peek(dut.io.round_out(x)(y)).toString(16)
     }
 
     if(arg.contentEquals("Verbose")){
@@ -216,100 +201,16 @@ class round_tester(dut: Sha3) extends PeekPokeTester(dut)  {
           "\t"+peekState(3)(y)+
           "\t"+peekState(4)(y)
         )
+      }
+   }
+
+  def initStateRegister():Unit={
+    for(x <- 0 to 4)
+      for(y <- 0 to 4)
+        poke(dut.io.register_in(x)(y),0x0000000000000000L)
+    step(1)
   }
-
-  def storeState():Unit={
-    pokeState(0)(0) = peek(dut.io.r_out(0))
-    pokeState(0)(1) = peek(dut.io.r_out(1))
-    pokeState(0)(2) = peek(dut.io.r_out(2))
-    pokeState(0)(3) = peek(dut.io.r_out(3))
-    pokeState(0)(4) = peek(dut.io.r_out(4))
-    pokeState(1)(0) = peek(dut.io.r_out(5))
-    pokeState(1)(1) = peek(dut.io.r_out(6))
-    pokeState(1)(2) = peek(dut.io.r_out(7))
-    pokeState(1)(3) = peek(dut.io.r_out(8))
-
-    pokeState(1)(4) = peek(dut.io.c_out(0))
-    pokeState(2)(0) = peek(dut.io.c_out(1))
-    pokeState(2)(1) = peek(dut.io.c_out(2))
-    pokeState(2)(2) = peek(dut.io.c_out(3))
-    pokeState(2)(3) = peek(dut.io.c_out(4))
-    pokeState(2)(4) = peek(dut.io.c_out(5))
-    pokeState(3)(0) = peek(dut.io.c_out(6))
-    pokeState(3)(1) = peek(dut.io.c_out(7))
-    pokeState(3)(2) = peek(dut.io.c_out(8))
-    pokeState(3)(3) = peek(dut.io.c_out(9))
-    pokeState(3)(4) = peek(dut.io.c_out(10))
-    pokeState(4)(0) = peek(dut.io.c_out(11))
-    pokeState(4)(1) = peek(dut.io.c_out(12))
-    pokeState(4)(2) = peek(dut.io.c_out(13))
-    pokeState(4)(3) = peek(dut.io.c_out(14))
-    pokeState(4)(4) = peek(dut.io.c_out(15))
-  }
-
-  def loadState():Unit={
-    poke(dut.io.r_in(0),pokeState(0)(0))
-    poke(dut.io.r_in(1),pokeState(0)(1))
-    poke(dut.io.r_in(2),pokeState(0)(2))
-    poke(dut.io.r_in(3),pokeState(0)(3))
-    poke(dut.io.r_in(4),pokeState(0)(4))
-    poke(dut.io.r_in(5),pokeState(1)(0))
-    poke(dut.io.r_in(6),pokeState(1)(1))
-    poke(dut.io.r_in(7),pokeState(1)(2))
-    poke(dut.io.r_in(8),pokeState(1)(3))
-
-    poke(dut.io.c_in(0), pokeState(1)(4))
-    poke(dut.io.c_in(1), pokeState(2)(0))
-    poke(dut.io.c_in(2), pokeState(2)(1))
-    poke(dut.io.c_in(3), pokeState(2)(2))
-    poke(dut.io.c_in(4), pokeState(2)(3))
-    poke(dut.io.c_in(5), pokeState(2)(4))
-    poke(dut.io.c_in(6), pokeState(3)(0))
-    poke(dut.io.c_in(7), pokeState(3)(1))
-    poke(dut.io.c_in(8), pokeState(3)(2))
-    poke(dut.io.c_in(9), pokeState(3)(3))
-    poke(dut.io.c_in(10), pokeState(3)(4))
-    poke(dut.io.c_in(11), pokeState(4)(0))
-    poke(dut.io.c_in(12), pokeState(4)(1))
-    poke(dut.io.c_in(13), pokeState(4)(2))
-    poke(dut.io.c_in(14), pokeState(4)(3))
-    poke(dut.io.c_in(15), pokeState(4)(4))
-  }
-
-  def initPokeState():Unit={
-
-    pokeState(0)(0) = 0x000000000000000dL
-    pokeState(1)(3) = 0x8000000000000000L
-    pokeState(0)(1) = 0x0000000000000000L
-    pokeState(0)(2) = 0x0000000000000000L
-    pokeState(0)(3) = 0x0000000000000000L
-    pokeState(0)(4) = 0x0000000000000000L
-    pokeState(1)(0) = 0x0000000000000000L
-    pokeState(1)(1) = 0x0000000000000000L
-    pokeState(1)(2) = 0x0000000000000000L
-    pokeState(1)(4) = 0x0000000000000000L
-    pokeState(2)(0) = 0x0000000000000000L
-    pokeState(2)(1) = 0x0000000000000000L
-    pokeState(2)(2) = 0x0000000000000000L
-    pokeState(2)(3) = 0x0000000000000000L
-    pokeState(2)(4) = 0x0000000000000000L
-    pokeState(3)(0) = 0x0000000000000000L
-    pokeState(3)(1) = 0x0000000000000000L
-    pokeState(3)(2) = 0x0000000000000000L
-    pokeState(3)(3) = 0x0000000000000000L
-    pokeState(3)(4) = 0x0000000000000000L
-    pokeState(4)(0) = 0x0000000000000000L
-    pokeState(4)(1) = 0x0000000000000000L
-    pokeState(4)(2) = 0x0000000000000000L
-    pokeState(4)(3) = 0x0000000000000000L
-    pokeState(4)(4) = 0x0000000000000000L
-  }
-
-
-
 }
-
-
 
 class round_spec extends FlatSpec with Matchers {
   "sha3" should "pass" in {
